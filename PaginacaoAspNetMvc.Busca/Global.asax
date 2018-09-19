7016200186dc34b4c049d284946e929c2445b8fa
Luiz Frederico Junior <luizfredericojr@msn.com>
2018-09-19 11:53:40 -0300

    first commit

---
+++ PaginacaoAspNetMvc.Busca/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PaginacaoAspNetMvc.Busca.MvcApplication" Language="C#" %>
---
+++ PaginacaoAspNetMvc/Controllers/ProdutosController.cs
@@ -0,0 +1,23 @@
+using PaginacaoAspNetMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using X.PagedList;
+
+namespace PaginacaoAspNetMvc.Controllers
+{
+    //Instalar via nuget: EntityFramework
+    //                    X.PagedList.MVC
+    public class ProdutosController : Controller
+    {
+		private LojaContext db = new LojaContext();
+        // GET: Produtos
+        public ActionResult Index(int pagina = 1)
+        {
+			var produtos = db.Produtos.OrderBy(p => p.Id).ToPagedList(pagina, 10);
+            return View(produtos);
+        }
+    }
+}
